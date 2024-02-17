8039f7c9c4ba25c7f00ae6bc07f7062ddd4a6ba7
Marquillos-99 <marckgit@gmail.com>
2024-02-17 10:55:18 -0600

    fruta

--- herencias/herenMult/comida.py
+++ herencias/herenMult/comida.py
@@ -1,5 +1,35 @@
 """
 Clase padre = Comida
 Clase Fruta hereda de Clase comida
-Clae postre hereda de clase  comida
+Clase postre hereda de clase  comida
 """
+class Comida:
+    def __init__(self,nombre,sabor):
+        self.nombre = nombre
+        self.sabor = sabor
+
+    def describe(self):
+        return f"{self.nombre} tiene sabor {self.sabor}"
+    
+class Fruta(Comida):
+    def __init__(self,nombre,sabor,color):
+        super().__init__(nombre,sabor)
+        self.color = color
+        
+    def describe(self):
+        return f"{self.nombre} es una fruta de color {self.color} y tiene sabor {self.sabor}"
+    
+class Postre(Comida):
+    def __init__(self,nombre,sabor,dulzura):
+        super().__init__(nombre,sabor)
+        self.dulzura = dulzura
+
+    def describe(self):
+        return "{} es un postre dulce con sabor {} y nivel de dulzura {}".format(self.nombre,self.sabor,self.dulzura)
+    
+
+manzana = Fruta("Manzana","dulce","roja")
+pastel_chocolate = Postre("Pastel Chocolate","rico","alta")
+
+print(manzana.describe())
+print(pastel_chocolate.describe())
